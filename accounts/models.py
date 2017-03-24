434be5c8cb5eadf71ab122e5815c1b16b8c04bb5
tvenko <venkotilen@gmail.com>
2017-03-24 21:48:12 +0100

    popravljena tabela za uporabnika, dodane vse potrebne tabele za pacienta, ne dela se

--- accounts/models.py
+++ accounts/models.py
@@ -1,21 +1,32 @@
+from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
 
-class SifraUstanove(models.Model):
-    """class representing institutions
+class Posta(models.Model):
 
-    This is a SifraUstanove Model containing name of institutions
+    stevilka = models.IntegerField(primary_key=True)
+    kraj = models.CharField(max_length=255)
+
+class SifraUstanove(models.Model):
+    """Razred, ki predstavlja zdravstvene ustanove
+
+    Model SifraUstanove, ki vsebuje ime zdravstvene ustanove.
     """
 
+    id = models.IntegerField(primary_key=True)
     naziv = models.CharField(max_length=255)
+    ulica = models.CharField(max_length=255)
+    hisna_stevilka = models.CharField(max_length=5)
+    posta = models.ForeignKey(Posta, on_delete=models.SET_NULL, null=True)
+
 
     #izpis na django admin panelu
     def __str__(self):
         return self.naziv
 
 class VrstaDelavca(models.Model):
-    """class representing type of workers
+    """Razred, ki predstavlja vrste delavcev
 
-    This is a VrstaDelavca Model containing type of workers
+    Model VrstaDelavca, ki vsebuje nazive tipov delavcev.
     """
 
     naziv = models.CharField(max_length=20)
@@ -24,23 +35,58 @@ class VrstaDelavca(models.Model):
     def __str__(self):
         return self.naziv
 
-class Delavec(models.Model):
-    """class representing all workers
+class SifraOkolisa(models.Model):
 
-    This is a Delavec Model containing information about workers email (also username),
-    first and second name, phone number, personal id, id of institution, and type of worker
-    (zdravnik, vodja PS, patronazna sestra,usluzbenec)
-    """
+    naziv = models.CharField(max_length=255)
 
-    osebna_sifra = models.IntegerField(primary_key=True)        # kako velika je lahko osebna sifra?
-    email = models.EmailField(max_length=255)
-    geslo = models.CharField(max_length=128)                    #to nisem cist ziher ce je kul tak
+
+class SorodstvenoRazmerje(models.Model):
+    naziv = models.CharField(max_length=30)
+
+class KontaktnaOseba(models.Model):
+
+
     priimek = models.CharField(max_length=30)
     ime = models.CharField(max_length=30)
-    tel = models.CharField(max_length=9)                        #so lahko tudi +386?
+    tel = models.CharField(max_length=13)
+    sorodstveno_razmerje = models.ForeignKey(SorodstvenoRazmerje, on_delete=models.SET_NULL, null=True)
+
+
+class Uporabnik(AbstractBaseUser):
+
+    email = models.EmailField(max_length=255, unique=True)
+    priimek = models.CharField(max_length=30)
+    ime = models.CharField(max_length=30)
+    tel = models.CharField(max_length=13)
+
+
+class Delavec(models.Model):
+    """Razred, ki predstavlja zdravtvene delavce
+
+    Model Delavec vsebuje informacije o emailu (hkratu uporabnisko ime), imeni in priimku,
+    telefonski stevilki, osebni stevilki, sifri zdravstvene ustanove in tipu delavca
+    (zdravnik, vodja PS, patronazna sestra,usluzbenec).
+    """
+
+    uporabnik = models.OneToOneField(Uporabnik, on_delete=models.CASCADE)
+
+    osebna_sifra = models.IntegerField(primary_key=True)
     sifra_ustanove = models.ForeignKey(SifraUstanove, on_delete=models.SET_NULL, null=True)
     vrsta_delavca = models.ForeignKey(VrstaDelavca, on_delete=models.SET_NULL, null=True)
 
     #izpis na django admin panelu
     def __str__(self):
         return self.ime+" "+self.priimek
+
+class Pacient(models.Model):
+
+
+    uporabnik = models.OneToOneField(Uporabnik, on_delete=models.CASCADE)
+
+    st_kartice = models.IntegerField(primary_key=True)
+    ulica = models.CharField(max_length=255)
+    posta = models.ForeignKey(Posta, on_delete=models.SET_NULL, null=True)
+    sifra_okolisa = models.ForeignKey(SifraOkolisa, on_delete=models.SET_NULL, null=True)
+    datum_rojstva = models.DateField()
+    spol = models.BooleanField()
+    kontaktna_oseba = models.ForeignKey(KontaktnaOseba, on_delete=models.SET_NULL, null=True)
