8fb1881fcb33442dcf14a38cea74a6a29a645430
tvenko <venkotilen@gmail.com>
2017-03-27 23:15:04 +0200

    dodane glavne tabele za delovniNalog, mankajo povezovalne tabele

--- delovniNalog/models.py
+++ delovniNalog/models.py
@@ -1,9 +1,66 @@
 from django.db import models
+from obisk.models import Obisk
+from accounts.models import Delavec, Pacient
 
 class VrstaObiska(models.Model):
+    """Razred, ki predstavlja sifrant vrst obiskov
+
+    Model VrstaObiska vsebuje sifro in opis vrste obiska.
+    """
 
     sifra = models.IntegerField(primary_key=True)
     opis = models.CharField(max_length=255)
 
     def __str__(self):
         return self.opis
+
+class Bolezen(models.Model):
+    """Razred, ki predstavlja sifrant bolezni
+
+    Model Bolezen vsebuje sifro in opis bolezni.
+    """
+
+    sifra = models.CharField(primary_key=True, max_length=10)
+    opis = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.opis
+
+class Material(models.Model):
+    """Razred, ki vsebuje sifrant materiala
+
+    Model Material vsebuje id in opis materiala.
+    """
+
+    opis = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.opis
+
+class Zdravilo(models.Model):
+    """Razred, ki vsebuje sifrant zdravil
+
+    Model Zdravilo vsebuje sifro, naziv in opis zdravila.
+    """
+
+    sifra = models.IntegerField(primary_key=True)
+    naziv = models.CharField(max_length=50)
+    opis = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.naziv
+
+class DelovniNalog(models.Model):
+    """Razred, ki vsebuje vse informacije o delovnem nalogu
+
+    Model DelovniNalog, vsebuje tuje kljuce sifro zdravnika, id pacienta, id obiska, sifro zdravila,
+    id materiala, sifro bolezni in sifro vrste obiska.
+    """
+
+    sifra_zdravnika = models.ForeignKey(Delavec, on_delete=models.SET_NULL, null=True) #kak naredit da bos lahk sam zdravnika zbral?
+    id_pacienta = models.ForeignKey(Pacient, on_delete=models.SET_NULL, null=True)
+    id_obiska = models.ForeignKey(Obisk, on_delete=models.SET_NULL, null=True)
+    sifra_zdravila = models.ForeignKey(Zdravilo, on_delete=models.SET_NULL, null=True)
+    id_materiala = models.ForeignKey(Material, on_delete=models.SET_NULL, null=True)
+    sifra_bolezni = models.ForeignKey(Bolezen, on_delete=models.SET_NULL, null=True)
+    sifra_vrste_obiska = models.ForeignKey(VrstaObiska, on_delete=models.SET_NULL, null=True)
--- obisk/models.py
+++ obisk/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+#se ni dokoncan -> stvar naslednjga sprinta zej se ne rabimo
+class Obisk(models.Model):
+    """Razred, ki bo vseboval podatke o obiskih
+
+    """
+
+    opis = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.opis
