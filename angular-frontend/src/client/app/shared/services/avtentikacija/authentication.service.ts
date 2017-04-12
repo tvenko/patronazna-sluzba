b7bbd6864cfb2b8b4449bfa713405024b2ccee39
David <david.rubin95@gmail.com>
2017-04-12 15:23:55 +0200

    Popravlejn API url, spremenjen header

--- angular-frontend/src/client/app/shared/services/avtentikacija/authentication.service.ts
+++ angular-frontend/src/client/app/shared/services/avtentikacija/authentication.service.ts
@@ -1,20 +1,23 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map'
- 
+import 'rxjs/add/operator/map';
+import { Config } from '../../config/env.config';
+
 @Injectable()
 export class AuthenticationService {
     public token: string;
- 
+
     constructor(private http: Http) {
         // set token if saved in local storage
         var currentUser = JSON.parse(localStorage.getItem('currentUser'));
         this.token = currentUser && currentUser.token;
     }
- 
+
     prijava(username: string, password: string): Observable<boolean> {
-        return this.http.post('http://localhost:8000/api/v1/token/auth/', JSON.stringify({ email: username, password: password }), {headers:{'Content-Type': 'application/json'}})
+      var headers = new Headers();
+      headers.append('Content-Type', 'application/json');
+        return this.http.post(Config.API + 'token/auth/', JSON.stringify({ email: username, password: password }), {headers: headers})
             .map(
 				(response : Response) => {
 					let token = response.json() && response.json().token;
@@ -34,13 +37,13 @@ export class AuthenticationService {
 			)
 			.catch(this.handleError);
     }
- 
+
     odjava(): void {
         // počisti token iz lokalnega pomnilnika
         this.token = null;
         localStorage.removeItem('currentUser');
     }
-	
+
 	private handleError (error: any) {
 		let errMsg = (error.message) ? error.message :
 		  error.status ? `${error.status} - ${error.statusText}` : 'Server error';
