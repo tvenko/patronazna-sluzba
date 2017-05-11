0a8a5951c1c7bd2f3cd8193df5ba2e1a4918951d
David <david.rubin95@gmail.com>
2017-05-11 17:49:48 +0200

    Dokončana #7, podrobnosti delovnega naloga

--- angular-frontend/src/client/app/podrobnosti-nalog/podrobnosti-nalog.component.ts
+++ angular-frontend/src/client/app/podrobnosti-nalog/podrobnosti-nalog.component.ts
@@ -12,10 +12,11 @@ import { DelovniNalogService } from '../shared/services/index';
   styleUrls: ['podrobnosti-nalog.component.css']
 })
 export class PodrobnostiNalogComponent implements OnInit {
-    //public idNaloga: any;
+    public idNaloga: any;
     public tipUporabnika: any;
     public delovniNalog: any;
     public errorNalaganja: any;
+    public errorCode: any;
 
     constructor(private activatedRoute: ActivatedRoute,
                 private delovniNalogService: DelovniNalogService,
@@ -27,6 +28,7 @@ export class PodrobnostiNalogComponent implements OnInit {
       // Pridobi id delovnega naloga iz naslova
       this.activatedRoute.params.subscribe((params: Params) => {
         let idNaloga = params['idNaloga'];
+        this.idNaloga = idNaloga;
         this.getDelovniNalog(idNaloga);
       });
 
@@ -39,10 +41,32 @@ export class PodrobnostiNalogComponent implements OnInit {
       this.delovniNalogService.get(id).subscribe(
         response => {
           this.delovniNalog = response;
+          if (this.delovniNalog.pacient.spol)
+            this.delovniNalog.pacient.spol = 'moški';
+          else
+            this.delovniNalog.pacient.spol = 'ženski';
+
+          if (this.delovniNalog.je_obvezen_datum)
+            this.delovniNalog.je_obvezen_datum = 'da';
+          else
+            this.delovniNalog.je_obvezen_datum = 'ne';
+
+          for (let obisk of this.delovniNalog.obiski) {
+            if (obisk.je_opravljen)
+              obisk.je_opravljen = 'da';
+            else
+              obisk.je_opravljen = 'ne';
+          }
+
+          for (let otrok of this.delovniNalog.vezani_pacienti) {
+            if (otrok.spol)
+              otrok.spol = 'moški';
+            else
+              otrok.spol = 'ženski';
+          }
         },
         error => {
           this.errorNalaganja = error;
-          console.log('Napaka pri pridobivanju delovnega naloga');
         }
       )
     }
