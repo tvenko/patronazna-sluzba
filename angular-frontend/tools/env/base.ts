41f1bc504876b2277f6f8a237799c1643a9f62a1
tvenko <venkotilen@gmail.com>
2017-05-26 00:55:15 +0200

    popravljen api url

--- angular-frontend/tools/env/base.ts
+++ angular-frontend/tools/env/base.ts
@@ -2,7 +2,7 @@ import { EnvConfig } from './env-config.interface';
 
 const BaseConfig: EnvConfig = {
   // Sample API url
-  //API: 'http://fruity-routy.ddns.net:3030/api/v1/'
+  API: 'http://fruity-routy.ddns.net:3030/api/v1/'
 };
 
 export = BaseConfig;
