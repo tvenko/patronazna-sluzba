6f9fc7a3e7535b24a89d9d1ca6a383467cbf15fe
vatavazz <vatavazz@users.noreply.github.com>
2017-05-29 18:05:32 +0200

    Sprememba podatkov pacienta

--- angular-frontend/src/client/app/sprememba-podatkov/sprememba-podatkov.component.ts
+++ angular-frontend/src/client/app/sprememba-podatkov/sprememba-podatkov.component.ts
@@ -17,8 +17,16 @@ export class SpremembaPodatkovComponent implements OnInit {
   public bap : any;
   public prikaziNapako : boolean;
   public prikaziPregled : boolean;
+  public si: any;
+  public poste: any;
+  public sifreOkolisa: any;
+  public sorodstvenaRazmerja: any;
+  public problemPridobivanja: boolean;
+
+  trenutniPodatki: any;
 
   public sForm: FormGroup;
+  public pForm: FormGroup;
 
   constructor(public pacientService: PacientService, private fb: FormBuilder) {}
 
@@ -40,12 +48,85 @@ export class SpremembaPodatkovComponent implements OnInit {
       geslo1: ['', Validators.required],
       geslo2: ['', Validators.required],
     });
+    this.pForm = this.fb.group({
+      datumRojstva: [''],
+      kraj: [''],
+      hisnaStevilka: [''],
+      ulica: [''],
+      eposta: [''],
+      ime: [''],
+      priimek: [''],
+      telefon: [''],
+      stevilkaPacienta: ['']
+    });
+    this.getPacient();
+    this.dobiSifre();
+    this.dobiPoste();
+
+    this.si = {
+            firstDayOfWeek: 0,
+            dayNames: ["Nedelja", "Ponedeljek", "Torek", "Sreda", "Četrtek", "Petek", "Sobota"],
+            dayNamesShort: ["Ned", "Pon", "Tor", "Sre", "Čet", "Pet", "Sob"],
+            dayNamesMin: ["Ne","Po","To","Sr","Če","Pe","So"],
+            monthNames: [ "Januar","Februar","Marec","April","Maj","Junij","Julij","Avgust","September","Oktober","November","December" ],
+            monthNamesShort: [ "Jan", "Feb", "Mar", "Apr", "Maj", "Jun","Jul", "Avg", "Sep", "Okt", "Nov", "Dec" ]
+    };
+  }
+
+  dobiSifre() {
+    this.problemPridobivanja = false;
+    this.pacientService.getSifreOkolisa()
+    .subscribe(
+      response => {
+        this.sifreOkolisa = response;
+      },
+      error => {
+        this.problemPridobivanja = true;
+      }
+    );
+  }
+
+  dobiPoste() {
+    this.problemPridobivanja = false;
+    this.pacientService.getPoste()
+      .subscribe(
+        response => {
+          this.poste = response;
+        },
+        error => {
+          this.problemPridobivanja = true;
+        }
+      )
+  }
+
+  getPacient() {
+    //console.log(this.uporabnik);
+    this.pacientService.get(this.pacient.stevilkaPacienta).subscribe(
+      response => {
+        this.trenutniPodatki = response;
+        this.trenutniPodatki.datumRojstva = new Date(this.trenutniPodatki.datumRojstva);
+        this.pForm.patchValue(response);
+      },error => {});
+  }
+
+  spremeniPodatke(podatki: any) {
+    podatki.datumRojstva = podatki.datumRojstva.toISOString().substr(0, 10);
+    //console.log(podatki);
+    //console.log(JSON.stringify(podatki).replace(/(\s*?{\s*?|\s*?,\s*?)(['"])?([a-zA-Z0-9]+)(['"])?:/g, '$1"$3":'));
+    podatki = JSON.stringify(podatki).replace(/(\s*?{\s*?|\s*?,\s*?)(['"])?([a-zA-Z0-9]+)(['"])?:/g, '$1"$3":');
+    console.log(podatki);
+    this.pacientService.spremeniPodatke(this.pacient.stevilkaPacienta, podatki).subscribe(
+      response => {
+        console.log("success");
+      },error => {
+        console.log("error");
+      });
   }
 
   spremeni(podatki: any) {
-    console.log(this.uporabnik);
+    //console.log(this.uporabnik);
     this.bap = {"novoGeslo" : podatki.geslo1, "staroGeslo" : podatki.trenutno};
-    console.log(this.bap);
+    //console.log(this.bap);
     this.pacientService.putGeslo(this.uporabnik.id, this.bap).subscribe(
       response => {
         console.log(response);
--- angular-frontend/src/client/app/sprememba-podatkov/sprememba-podatkov.module.ts
+++ angular-frontend/src/client/app/sprememba-podatkov/sprememba-podatkov.module.ts
@@ -6,11 +6,11 @@ import { SharedModule } from '../shared/shared.module';
 import { AlertModule } from 'ng2-bootstrap/alert';
 import { EqualValidator } from './validacija';
 import { ReactiveFormsModule } from '@angular/forms';
-import { DialogModule, ButtonModule } from 'primeng/primeng';
+import { DialogModule, ButtonModule, CalendarModule } from 'primeng/primeng';
 
 @NgModule({
   imports: [SpremembaPodatkovRoutingModule, SharedModule.forRoot(), AlertModule.forRoot(),
-    ReactiveFormsModule, DialogModule, ButtonModule ],
+    ReactiveFormsModule, DialogModule, ButtonModule,CalendarModule ],
   declarations: [SpremembaPodatkovComponent, EqualValidator],
   exports: [SpremembaPodatkovComponent]
 })
